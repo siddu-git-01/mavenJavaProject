6360f72af348a724f69b3121c72aacc8149f42e4
sai siddhardha <saisiddhardhapodili@gmail.com>
2024-11-16 15:35:12 +0530

    initial commit

---
+++ src/main/java/SEGroup/mavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.mavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
